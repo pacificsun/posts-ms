c6b1487e60cf2aeed9b7411ffcbdeadf738cb1fd
pacificsuraj <stsurajst@gmail.com>
2021-05-01 23:27:36 +0545

    query service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
-
+const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
@@ -11,12 +11,13 @@ app.use(express.json());
 
 app.use(cors());
 const posts = {};
+// {{id1},{id2}}
 
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   console.log("here>>", title);
@@ -24,7 +25,19 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => console.log("Listening on 4000"));
